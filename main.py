0d3e355e528769ab72baca7d4a5a06e2d8de0c62
whoAshishSaini <ashishshan97@gmail.com>
2025-10-16 00:08:13 +0530

    Hello world project

--- main.py
+++ main.py
@@ -1,13 +1,41 @@
 import os
 
 from dotenv import load_dotenv
-
+from langchain_groq import ChatGroq
+from langchain_core.prompts import PromptTemplate
+from langchain_ollama import ChatOllama
 load_dotenv()
 
 
 def main():
     print("Hello from ud-langchain-course!")
-    print(os.environ.get("GROQ_API_KEY"))
+    information = """
+    Elon Reeve Musk (/ˈiːlɒn/ EE-lon; born June 28, 1971) is a businessman and entrepreneur known for his leadership of Tesla, SpaceX, Twitter, and xAI. Musk has been the wealthiest person in the world since 2021; as of October 2025, Forbes estimates his net worth to be US$500 billion.
+
+Born into a wealthy family in Pretoria, South Africa, Musk emigrated in 1989 to Canada; he had obtained Canadian citizenship at birth through his Canadian-born mother. He received bachelor's degrees in 1997 from the University of Pennsylvania in Philadelphia, United States, before moving to California to pursue business ventures. In 1995, Musk co-founded the software company Zip2. Following its sale in 1999, he co-founded X.com, an online payment company that later merged to form PayPal, which was acquired by eBay in 2002. That year, Musk also became an American citizen.
+
+In 2002, Musk founded the space technology company SpaceX, becoming its CEO and chief engineer; the company has since led innovations in reusable rockets and commercial spaceflight. Musk joined the automaker Tesla as an early investor in 2004 and became its CEO and product architect in 2008; it has since become a leader in electric vehicles. In 2015, he co-founded OpenAI to advance artificial intelligence (AI) research, but later left; growing discontent with the organization's direction and their leadership in the AI boom in the 2020s led him to establish xAI. In 2022, he acquired the social network Twitter, implementing significant changes, and rebranding it as X in 2023. His other businesses include the neurotechnology company Neuralink, which he co-founded in 2016, and the tunneling company the Boring Company, which he founded in 2017.
+    """
+
+    summary_template = """
+    Given the information {information} about a person I want you to:
+    1) Create a short summary of the person
+    2) List two interesting facts about the person
+    """
+
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
+
+    llm_groq = ChatGroq(model="llama-3.1-8b-instant",
+    temperature=0.0)
+
+    llm_ollama = ChatOllama(model="gemma3:4b",temperature=0.0)
+
+    chain=summary_prompt_template | llm_groq
+    response = chain.invoke(input={"information": information})
+    print(response.content)
+
 
 
 if __name__ == "__main__":
